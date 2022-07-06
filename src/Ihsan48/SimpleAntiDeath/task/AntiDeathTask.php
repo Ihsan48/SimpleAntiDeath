7954a7c2efd7b2a7a5b63c8a369406d4f0019da0
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-07-06 23:56:51 +0700

    Bruh

--- src/Ihsan48/SimpleAntiDeath/task/AntiDeathTask.php
+++ src/Ihsan48/SimpleAntiDeath/task/AntiDeathTask.php
@@ -8,7 +8,7 @@ use Ihsan48\SimpleAntiDeath\AntiDeath;
 class AntiDeathTask extends Task {
 
     public function __construct(private AntiDeath $plugin) {
-        $this->plugin = $plugin
+        $this->plugin = $plugin;
     }
 
     public function onRun() : void {
