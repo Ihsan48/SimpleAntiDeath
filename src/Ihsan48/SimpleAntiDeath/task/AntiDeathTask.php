386707cede0508467496270e263ea2fe7c850958
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-07-06 23:56:01 +0700

    Update AntiDeathTask.php

---
+++ src/Ihsan48/SimpleAntiDeath/task/AntiDeathTask.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ihsan48\SimpleAntiDeath\task;
+
+use pocketmine\scheduler\Task;
+
+use Ihsan48\SimpleAntiDeath\AntiDeath;
+
+class AntiDeathTask extends Task {
+
+    public function __construct(private AntiDeath $plugin) {
+        $this->plugin = $plugin
+    }
+
+    public function onRun() : void {
+        foreach ($this->plugin->getServer()->getWorldManager()->getWorldByName($this->plugin->enableInWorlds)->getPlayers() as $players) {
+            $players->setAirSupplyTicks($players->getMaxAirSupplyTicks());
+        }
+    }
+} 
